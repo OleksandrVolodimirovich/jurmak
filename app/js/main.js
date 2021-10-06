fd212a1be839c9e4ac65c0c6baaae4d44ff3e521
Alexandr <net_box@ukr.net>
2021-10-06 15:12:07 +0300

    init: create jurmak ver.01

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        slidesToShow: 1,
+        autoplaySpeed: 4000
+    });
+});
+
+
+
